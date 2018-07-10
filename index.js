743b0c0c02534815133092175ba9a20945f5935b
Daniel Griffiths <logiconictvtube@gmail.com>
2018-07-09 20:55:00 -0400

    trying new fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 require('./models/User')
 require('./services/passport');
@@ -27,9 +28,8 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
