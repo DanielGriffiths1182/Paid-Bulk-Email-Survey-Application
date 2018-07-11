cd624e6d61b70b29432238e9d129dd98a17dca4a
Daniel Griffiths <logiconictvtube@gmail.com>
2018-07-10 22:38:34 -0400

    integrating email client for survey distribution

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-require('./models/User')
+require('./models/User');
+require('./models/Survey');
+
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -26,6 +28,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
